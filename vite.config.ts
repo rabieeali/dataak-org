46771c5e28cf861c66bacaedbaaafb34a2314f49
ali.rabiee <a.rabiee@fidilio.com>
2025-03-07 20:56:31 +0330

    feature:anwser page

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { compression } from 'vite-plugin-compression2'
+import ViteImagemin from 'vite-plugin-imagemin';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'brotliCompress',
+      threshold: 10240
+    }),
+    ViteImagemin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 85 },
+      pngquant: { quality: [0.65, 0.9] },
+      svgo: { plugins: [{ removeViewBox: false }] },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+  build: {
+    minify: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+  },
+});
